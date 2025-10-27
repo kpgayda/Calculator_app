f6fa3b7c7531bab9101e58f48d1809f20366e983
KevinGayda <kevingayda@gmail.com>
2025-10-27 11:19:49 -0700

    calculator commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const screen = document.querySelector(".screen_text");
+const digits = document.querySelectorAll(".digit");
+const operators = document.querySelectorAll(".operator");
+const equals = document.querySelector(".equal_sign");
+
+const first = digits.forEach(function (screen) {
+  screen.addEventListener("click", function () {
+    addDigit(screen.textContent);
+  });
+});
+
+function addDigit(digit) {
+  screen.textContent = screen.textContent + digit;
+}
+
+operators.forEach(function (screen) {
+  screen.addEventListener("click", function () {
+    addOperator(screen.textContent);
+  });
+});
+
+function addOperator(operator) {
+  screen.textContent = screen.textContent + operator;
+}
+
+equals.addEventListener("click", function () {
+  //console.log(first.value);
+});
